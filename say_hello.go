0be461d66439f5cfe2622174c3ff777ba12f8664
Ayub Subagiya <ayub.subagiya.tik21@mhsw.pnj.ac.id>
2025-12-10 09:03:57 +0700

    update say hello with parameter name

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello World ayub subagiya sedang belajar golang"
+func SayHello(name string) string {
+	return "Hello World sedang belajar golang" + name + "!"
 }
